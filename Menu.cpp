0a63572b609729ca513ddf7d05f025e0bce43c31
= <=>
2016-12-14 02:10:31 +0300

    Наконец на русском, ёпта. Добавлены пункты меню и переключение

--- Menu.cpp
+++ Menu.cpp
@@ -4,10 +4,13 @@
 #include <GL/freeglut.h>
 #include "Menu.h"
 #include <iostream>
+#include <string>
+#include <cmath>
 
-Menu::Menu(std::string _title, Controller& _c): title(_title), position(1), c(_c)
+Menu::Menu(std::string _title, Controller& _c): title(_title), position(0), c(_c)
 {
-	
+	items.push_back("NEW GAME");
+	items.push_back("EXIT");
 }
 
 void Menu::Keyboard(int key)
@@ -17,7 +20,14 @@ void Menu::Keyboard(int key)
 
 void Menu::SpecialKeyboard(int key) 
 {
-      
+	switch(key) {
+		case GLUT_KEY_UP:
+			position = abs(position-1) % items.size();
+		break;
+		case GLUT_KEY_DOWN:
+			position = abs(position+1) % items.size();
+		break;   
+	}
 }
 
 void Menu::Render()
@@ -27,5 +37,24 @@ void Menu::Render()
 
 	glColor3ub( 0, 222, 18);
     glRasterPos3d(WIDTH/2-title.length()*4.5, HEIGHT - 30, 0);
-    glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"TANKS ELITE");
+    glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)title.c_str());
+
+    int offset_y = 250;
+    int i = 0;
+    for(std::string& s: items)
+    {
+    	if(i == position)
+    	{
+    		glRasterPos3d(WIDTH/2-s.length()*4.5 - 40, HEIGHT - offset_y, 0);
+    		glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)">");
+    		glRasterPos3d(WIDTH/2+s.length()*3.5 + 40, HEIGHT - offset_y, 0);
+    		glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)"<");
+    	}
+    	glRasterPos3d(WIDTH/2-s.length()*4.5, HEIGHT - offset_y, 0);
+    	glutBitmapString(GLUT_BITMAP_9_BY_15, (unsigned char*)s.c_str());
+    	offset_y += 40;
+    	i++;
+    }
+    
+
 }
